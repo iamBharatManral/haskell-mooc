1defd8ffc596fb6901357c67c22d27f03ceab793
iamBharatManral <justbharatmanral@gmail.com>
2024-05-18 23:46:12 +0530

    exercise: set 2b complete

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -16,7 +16,10 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial _ 0 = 1
+binomial n k
+  | n == 0 && k > 0 = 0
+  | otherwise = binomial (n - 1) k + binomial (n - 1) (k - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
@@ -27,17 +30,25 @@ binomial = todo
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial n
+  | n == 1 = 1
+  | even n = oddFactorial (n - 1)
+  | otherwise = n * oddFactorial (n - 2)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
 -- common divisor:
 --
 -- Given two numbers, a and b,
+
 -- * if one is zero, return the other number
+
 -- * if not, subtract the smaller number from the larger one
+
 -- * replace the larger number with this new number
+
 -- * repeat
+
 --
 -- For example,
 --   myGcd 9 12 ==> 3
@@ -56,10 +67,15 @@ oddFactorial = todo
 --   0      3
 --
 -- Background reading:
+
 -- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
 myGcd :: Integer -> Integer -> Integer
-myGcd = todo
+myGcd a b
+  | a == 0 = b
+  | b == 0 = a
+  | a < b = myGcd b a
+  | otherwise = myGcd (a - b) b
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the function leftpad which adds space characters
@@ -71,11 +87,16 @@ myGcd = todo
 --   leftpad "xxxxx" 3 ==> "xxxxx"
 --
 -- Tips:
+
 -- * you can combine strings with the ++ operator.
+
 -- * you can compute the length of a string with the length function
 
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad s l = spaces ++ s
+ where
+  spaces = concat $ map (\_ -> " ") [0 .. remaining - 1]
+  remaining = l - length s
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
@@ -86,12 +107,19 @@ leftpad = todo
 --   countdown 4 ==> "Ready! 4... 3... 2... 1... Liftoff!"
 --
 -- Hints:
+
 -- * you can combine strings with the ++ operator
+
 -- * you can use the show function to convert a number into a string
+
 -- * you'll probably need a recursive helper function
 
 countdown :: Integer -> String
-countdown = todo
+countdown n = "Ready! " ++ output ++ " Liftoff!"
+ where
+  output = count n
+  count 1 = show 1 ++ "... "
+  count n = show n ++ "... " ++ count (n - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement the function smallestDivisor that returns the
@@ -109,7 +137,9 @@ countdown = todo
 -- Hint: remember the mod function!
 
 smallestDivisor :: Integer -> Integer
-smallestDivisor = todo
+smallestDivisor n = divisor n 2
+ where
+  divisor n c = if n `mod` c == 0 then c else divisor n (c + 1)
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a function isPrime that checks if the given number
@@ -118,7 +148,9 @@ smallestDivisor = todo
 -- Ps. 0 and 1 are not prime numbers
 
 isPrime :: Integer -> Bool
-isPrime = todo
+isPrime n
+  | n == 0 || n == 1 = False
+  | otherwise = if smallestDivisor n == n then True else False
 
 ------------------------------------------------------------------------------
 -- Ex 8: implement a function biggestPrimeAtMost that returns the
@@ -133,4 +165,6 @@ isPrime = todo
 --   biggestPrimeAtMost 10 ==> 7
 
 biggestPrimeAtMost :: Integer -> Integer
-biggestPrimeAtMost = todo
+biggestPrimeAtMost n = biggest n
+ where
+  biggest n = if isPrime n then n else biggest (n - 1)
